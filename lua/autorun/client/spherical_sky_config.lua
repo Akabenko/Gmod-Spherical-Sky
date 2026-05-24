0055c006b00430ab564e9525bad7d29e689a3dbd
Evgeny Akabenko <danlimons322@gmail.com>
2026-05-24 14:01:58 +0300

    Create spherical_sky_config.lua

---
+++ lua/autorun/client/spherical_sky_config.lua
@@ -0,0 +1,13 @@
+
+hook.Add("ConfigSphericalSky", "DefaultSky", function()
+	return {
+		"sky/qwantani_dusk_2_puresky_4k";
+		"sky/citrus_orchard_road_puresky_4k";
+		"sky/kloppenheim_07_puresky_8k";
+		"sky/sunflowers_puresky_4k";
+		"sky/qwantani_night_puresky_4k";
+		-- add your name skybox here
+		-- https://polyhaven.com/hdris/skies
+	}
+end)
+
